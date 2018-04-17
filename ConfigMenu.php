03fec1aa5c00de57db51aa88241aae7aac7d7634
Václav Pelíšek <peldax@gmail.com>
2018-04-18 00:53:31 +0200

    Add files via upload

---
+++ ConfigMenu.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types = 1);
+
+namespace Nepttune\Component;
+
+final class ConfigMenu extends BaseComponent
+{
+    /** @var array */
+    protected $menu;
+
+    public function __construct(array $menu)
+    {
+        $this->menu = $menu;
+    }
+
+    protected function beforeRender() : void
+    {
+        $this->template->menu = $this->menu;
+    }
+}
